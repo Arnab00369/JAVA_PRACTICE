70c5b2930c8e71fa43587fffcaa2382051ee6e32
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-12 07:05:46 +0000

    File done

--- KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Program_1.java
+++ KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Program_1.java
@@ -1,1 +1,10 @@
-import java.util.
+import java.util.lang.*; //importing package
+
+public class First Program //defining class
+{ 
+    public static void main(String args[])//defining main function
+    {
+        //Display or printing message
+        System.out.println("Welcome to the First Java Program");
+    }
+}
