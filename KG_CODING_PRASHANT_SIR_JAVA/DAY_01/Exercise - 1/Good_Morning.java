f377a68f0acb4bc13a2f4a374facbff9958a4724
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-15 17:09:27 +0000

    EXERCISE-1(PROGRAM_1)

---
+++ KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Good_Morning.java
@@ -0,0 +1,7 @@
+//Create a class and output "Good Morning"
+import java.util.*;
+public class Good_Morning{
+    public static void main(String args[]){
+        System.out.println("Good Morning, Arnab!!!");
+    }
+}
