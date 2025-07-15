280eca637817f53ec82fc731a54373a935621705
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-15 17:38:29 +0000

    Done

--- KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Reverse_Right_Half_Pyramid.java
+++ KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Reverse_Right_Half_Pyramid.java
@@ -12,10 +12,10 @@ public class Reverse_Right_Half_Pyramid{
     public static void main(String[] args) {
         System.out.println("The Reverse Right Half Pyramid Pattern is as follows::");
         System.out.println("* * * * *");
-        System.out.println("  * * * *");
-        System.out.println("    * * *");
-        System.out.println("      * *");
-        System.out.println("        *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
         
         
         
