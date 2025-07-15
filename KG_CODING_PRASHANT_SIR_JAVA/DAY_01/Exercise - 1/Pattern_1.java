8bab1810e8d2255387dc41d02b9ebbab6328d37c
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-15 17:17:52 +0000

    ok

--- KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Pattern_1.java
+++ KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Pattern_1.java
@@ -6,13 +6,13 @@
  * * * * *
 */
 import java.lang.*;
-public class Pattern_1{
+public class Left_Half_Pyramid{
     public static void main(String[] args) {
         System.out.println("The Left Half Pyramid Pattern is as follows::");
         System.out.println("*");
         System.out.println("* *");
         System.out.println("* * *");
-        System.out.println("****");
-        System.out.println("*****");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
     }
 }
