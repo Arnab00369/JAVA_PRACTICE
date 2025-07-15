716531b1a1073985f877ae533b584a4179aad19d
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-15 17:42:58 +0000

    4th pattern done

---
+++ KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Reverse_Left_Half_Pyramid.java
@@ -0,0 +1,20 @@
+/*Print the following pattern using print function::
+  * * * * *
+    * * * *
+      * * *
+        * *
+          *
+  
+ */
+
+import java.lang.*;
+public class Reverse_Left_Half_Pyramid{
+    public static void main(String[] args) {
+        System.out.println("The Reverse Left Half Pyramid Pattern is as follows::");
+        System.out.println("* * * * *");
+        System.out.println("  * * * *");
+        System.out.println("    * * *");
+        System.out.println("      * *");
+        System.out.println("        *");    
+    }
+}
