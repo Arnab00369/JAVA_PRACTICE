aaec881bd2f02c2e2109f49f930d2f939bb54290
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-15 17:24:19 +0000

    Necessary changes made

--- KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Right_Half_Pyramid.java
+++ KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Right_Half_Pyramid.java
@@ -10,7 +10,7 @@
 import java.lang.*;
 public class Right_Half_Pyramid{
     public static void main(String[] args) {
-        System.out.println("The  Half Pyramid Pattern is as follows::");
+        System.out.println("The Right Half Pyramid Pattern is as follows::");
         System.out.println("        *");
         System.out.println("      * *");
         System.out.println("    * * *");
