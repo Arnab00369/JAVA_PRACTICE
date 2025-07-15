770499d4cc2d59a397e9464b229f41b9531c406f
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-15 17:15:17 +0000

    Chages done pattern star

--- KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Pattern_1.java
+++ KG_CODING_PRASHANT_SIR_JAVA/DAY_01/Exercise - 1/Pattern_1.java
@@ -1,7 +1,17 @@
-
+/*Print the following pattern using print function::
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+*/
 import java.lang.*;
 public class Pattern_1{
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
     }
 }
